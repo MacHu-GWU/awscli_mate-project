e7b449828d5ed1e2797e6b52a37afea8da0116f8
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-04-04 15:20:04 -0400

    Create aws_mfa_auth.py

---
+++ aws_mfa_auth.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+A simple CLI tool to help you to do MFA authentication with AWS CLI.
+
+Usage:
+
+.. code-block:: bash
+
+    python aws_mfa_auth.py --base_profile your_profile --mfa_token 123456
+"""
+
+import fire
+from awscli_mate.awscli import AWSCliConfig
+
+awscli_config = AWSCliConfig()
+
+
+def auth(
+    base_profile: str,
+    mfa_token: str,
+):
+    awscli_config.mfa_auth(
+        profile=base_profile,
+        mfa_code=str(mfa_token),
+        overwrite_default=True,
+    )
+
+
+if __name__ == "__main__":
+    fire.Fire(auth)
